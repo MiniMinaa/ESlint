c03374b3f8e5f16b4b91eeabfcaa5eaa1f91fe84
Mina R <minarumang@icloud.com>
2026-02-11 17:05:20 +0100

    task 4-6

--- index.ts
+++ index.ts
@@ -17,3 +17,25 @@ const isEven = (num: number) => {
   return num % 2 === 0;
 };
 console.log(isEven(4));
+
+//Task four
+
+const square = (x: number) => {
+  return x * x;
+};
+console.log(`square is ${square(5)}`);
+
+//Task five
+
+const getAge = (year: number) => {
+  return 2026 - year;
+};
+console.log(`This year im turning ${getAge(2001)} years old.`);
+
+//Task six
+const prices = [10, 20, 30];
+let total = 0;
+prices.forEach((p) => {
+  total += p;
+});
+console.log(`the total is ${total}`);
